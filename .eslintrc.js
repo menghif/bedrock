30d199366f0732297002c8b217e8b7034e0444f7
Alex Gibson <alexgibson@users.noreply.github.com>
2021-10-06 13:18:46 +0100

    Format JS files using Prettier (Fixes #10468, #10479)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,34 +6,13 @@ module.exports = {
     },
     extends: [
         'eslint:recommended',
-        'plugin:json/recommended'
+        'plugin:json/recommended',
+        'prettier'
     ],
     rules: {
         // Require strict mode directive in top level functions
         // https://eslint.org/docs/rules/strict
         'strict': ['error', 'function'],
-
-        // This option sets a specific tab width for your code
-        // https://eslint.org/docs/rules/indent
-        'indent': ['error', 4],
-
-        // Disallow mixed 'LF' and 'CRLF' as linebreaks
-        // https://eslint.org/docs/rules/linebreak-style
-        'linebreak-style': ['error', 'unix'],
-
-        // Specify whether double or single quotes should be used
-        'quotes': ['error', 'single'],
-
-        // Require or disallow use of semicolons instead of ASI
-        'semi': ['error', 'always'],
-
-        // Enforce location of semicolons
-        // https://eslint.org/docs/rules/semi-style
-        'semi-style': ['error', 'last'],
-
-        // Require camel case names
-        // https://eslint.org/docs/rules/camelcase
-        'camelcase': ['error', { 'properties': 'always' }],
 
         // Use type-safe equality operators
         // https://eslint.org/docs/rules/eqeqeq
@@ -42,14 +21,6 @@ module.exports = {
         // Treat var statements as if they were block scoped
         // https://eslint.org/docs/rules/block-scoped-var
         'block-scoped-var': 'error',
-
-        // Require newlines around variable declarations
-        // https://eslint.org/docs/rules/one-var-declaration-per-line
-        'one-var-declaration-per-line': ['error', 'always'],
-
-        // Require constructor names to begin with a capital letter
-        // https://eslint.org/docs/rules/new-cap
-        'new-cap': 'error',
 
         // Disallow Use of alert, confirm, prompt
         // https://eslint.org/docs/rules/no-alert
